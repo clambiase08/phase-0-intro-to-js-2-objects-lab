3c73c4db05f47b698cd65997bcdcc6ed5b263be3
Christina Lambiase <christina.lambiase@gmail.com>
2023-02-26 18:05:04 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+const employee = {
+    name : "Xtina",
+    streetAddress : "100 West 5th St",
+}
+
+function updateEmployeeWithKeyAndValue(employee, streetAddress) {
+    return {...employee, [streetAddress]: "11 Broadway",
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress) {
+    employee [streetAddress] = "12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee) {
+    const deleteFromEmployeeByKey = {...employee};
+    delete deleteFromEmployeeByKey.name;
+    return deleteFromEmployeeByKey;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee) {
+    delete employee.name;
+    return employee;
+}
